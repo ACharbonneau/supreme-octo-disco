9c84498a8d59173bb07dac2d10bd96192fa108b6
Amanda Charbonneau <charbo24@msu.edu>
2016-09-15 17:38:26 -0400

    initial commit

---
+++ 1_GetData.sh
@@ -0,0 +1,50 @@
+echo "1. Copying Files"
+
+mkdir RawFastq & cd RawFastq
+
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C6G98ANXX_8_fastq.gz > C6G98ANXX_8.fastq
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C6P86ANXX_4_fastq.gz > C6P86ANXX_4.fastq
+
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C81E4ANXX_1_fastq.gz > C81E4ANXX_1.fastq
+
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C81FNANXX_1_fastq.gz > C81FNANXX_1.fastq
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C81FNANXX_2_fastq.gz > C81FNANXX_2.fastq
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C81FNANXX_3_fastq.gz > C81FNANXX_3.fastq
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C81FNANXX_4_fastq.gz > C81FNANXX_4.fastq
+
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C81KHANXX_8_fastq.gz > C81KHANXX_8.fastq
+
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C81LCANXX_2_fastq.gz > C81LCANXX_2.fastq
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C81LCANXX_3_fastq.gz > C81LCANXX_3.fastq
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C81LCANXX_4_fastq.gz > C81LCANXX_4.fastq
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C81LCANXX_5_fastq.gz > C81LCANXX_5.fastq
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C81LCANXX_6_fastq.gz > C81LCANXX_6.fastq
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C81LCANXX_7_fastq.gz > C81LCANXX_7.fastq
+gunzip -c  /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/C81LCANXX_8_fastq.gz > C81LCANXX_8.fastq
+
+echo "2. Getting MetaData"
+
+cd ../
+cp -r /mnt/research/radishGenomics/OriginalSequencingFiles/GBS_Cornell_2015/Metadata/ .
+cd Metadata/
+mv QTL_F2_8.txt C6G98ANXX_8.fastq.gz.keys.txt
+for i in `ls *fastq.gz.keys.txt`; do cut -f 3,4 ${i} | tail -n +2 > `echo ${i} | sed s/_fastq.gz.keys.txt/.barcodes/` ; done
+
+echo "3. Setting up workspace"
+
+cd ../
+mkdir ProcessRadtags
+mkdir SigSelection
+mkdir AE_Deconvoluted
+
+echo "4. Launching fastqc"
+
+mkdir fastQC
+mkdir fastQC/RawFQC
+mkdir fastQC/TrimmedFQC
+cd fastQC/RawFQC
+
+qsub supreme-octo-disco/2_FastQC.qsub
+
+
+
