33e3000d9bb67215794b1104876825820d314ff1
Amanda Charbonneau <charbo24@msu.edu>
2016-11-17 14:45:40 -0500

    fixed typo in path

--- 1_GetData.sh
+++ 1_GetData.sh
@@ -27,8 +27,9 @@ cd Metadata/PlateInfoSeq/
 mv QTL_F2_8.txt C6G98ANXX_8_fastq.gz.keys.txt
 for i in `ls *fastq.gz.keys.txt`; do cut -f 3,4 ${i} | tail -n +2 > `echo ${i} | sed s/_fastq.gz.keys.txt/.barcodes/` ; done
 cd ../../supreme-octo-disco/
+module load R/3.2.0
 R --file=1.1_metadatamunge.R
-cd -
+
 
 echo "3. Setting up workspace"
 
