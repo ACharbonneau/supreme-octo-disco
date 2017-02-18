7fca799b89c42bebbe0baa74243930e7524c4a94
Amanda Charbonneau <charbo24@msu.edu>
2017-02-18 18:21:10 -0500

    YEIL has wrong species in metadata, r code replaces it

--- 1.1_metadatamunge.R
+++ 1.1_metadatamunge.R
@@ -51,6 +51,7 @@ DNA_data <- read.csv("../Metadata/OriginalFiles/MetadataAll.txt", sep = "\t", he
 
 colnames(DNA_data) <- c("ID", "Type_Year", "Species", "Cross", "Indiv", "Date", "Prep")
 
+DNA_data$Species[DNA_data$Cross=="YEIL_CLNC"] <- "Rros"
 DNA_data$Cross[DNA_data$Cross=="SPEU"] <- "SPNK"
 
 DNA_data <- droplevels(DNA_data)
