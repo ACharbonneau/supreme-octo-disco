1bdf8eb838a2755e999392567528054403a07551
Amanda Charbonneau <charbo24@msu.edu>
2017-02-27 17:40:42 -0500

    made JustPops more general

--- 1.1_metadatamunge.R
+++ 1.1_metadatamunge.R
@@ -51,6 +51,7 @@ DNA_data <- read.csv("../Metadata/OriginalFiles/MetadataAll.txt", sep = "\t", he
 
 colnames(DNA_data) <- c("ID", "Type_Year", "Species", "Cross", "Indiv", "Date", "Prep")
 
+# File from Jeff has all of these wrong :(
 DNA_data$Species[DNA_data$Cross=="SAES"] <- "Rrl"
 DNA_data$Species[DNA_data$Cross=="RA808"] <- "Rrl"
 DNA_data$Species[DNA_data$Cross=="YEIL_CLNC"] <- "Rros"
