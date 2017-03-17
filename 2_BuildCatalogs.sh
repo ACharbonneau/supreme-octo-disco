7d63e2e02ee4262d14aaecb6e0f9734f39353c09
Amanda Charbonneau <charbo24@msu.edu>
2017-03-17 11:35:03 -0400

    fixed code to use new files with flags

--- 2_BuildCatalogs.sh
+++ 2_BuildCatalogs.sh
@@ -25,29 +25,20 @@ expandedfiles=
 ID=0
 
 for file in ${files};
-    do needsexpand=`cat ${file}`
-    batchID="${dt}${ID}" 
-    for unexpandableline in ${needsexpand}
-        do echo " -s " ls ${unexpandableline}.tags.tsv | sed "s/.tags.tsv//" | sed "s/ls //" 
-    done | tr -d '\n' > ${file}.expanded
-    qsub -N `basename ${file}`.expanded -v InputFile="${file}.expanded",outputfolder=`basename ${file}`,batchID="${batchID}" ../../../supreme-octo-disco/2.1_cs_stacks.qsub
+    do batchID="${dt}${ID}" 
+    qsub -N `basename ${file}`.expanded -v InputFile="${file}",outputfolder=`basename ${file}`,batchID="${batchID}" ../../../supreme-octo-disco/2.1_cs_stacks.qsub
     ID=`expr ${ID} + 1`
 done
 
 
-
-
-
 # Assuming it's a cross
-
 
 cp ../../../supreme-octo-disco/2.1_cs_stacks.qsub ../../../supreme-octo-disco/2.1_cs_mapping.qsub
 
-
 sed -i "s/sstacks.*//" ../../../supreme-octo-disco/2.1_cs_mapping.qsub
 
-echo 'sstacks -g -p 20 -b ${batchID} -c ${batchID} -s `cat AE_F2_cs_stacks_list` -o . F2_F0_Mapping.log' >> ../../../supreme-octo-disco/2.1_cs_mapping.qsub
+echo 'sstacks -g -p 20 -b ${batchID} -c ${batchID} -s `cat AE_Mapping_cs_stacks_list` -o . F0_F2_Mapping.log' >> ../../../supreme-octo-disco/2.1_cs_mapping.qsub
 
 batchID="${dt}${ID}"
 
-qsub -N Mapping_F0_F2 -v InputFile="../../../Metadata/AE_F0_cs_stacks_list.expanded",outputfolder="AE_F0_F1",batchID="${batchID}" ../../../supreme-octo-disco/2.1_cs_stacks.qsub
+qsub -N Mapping_F0_F2 -v InputFile="../../../Metadata/AE_Mapping_cs_stacks_list",outputfolder="AE_F0_F2",batchID="${batchID}" ../../../supreme-octo-disco/2.1_cs_stacks.qsub
