f5e544a0966107b0df11eff02a48d62de1adb401
Amanda Charbonneau <charbo24@msu.edu>
2017-03-31 14:31:02 -0400

    auto launch structure

---
+++ 1.5_STRUCTURE_submit.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+#Launches 20 different randomizations of the dataset (seq 1 20) with a K of ( -t <number range>)
+
+#BATCH="batch_20170329.structure.tsv"
+
+
+#Randomize Input files
+tail -n +2 ${BATCH} > nohead_${BATCH}
+
+NLINES=$(tail -n +2 nohead_${BATCH} | wc -l )
+INDIVIDS=`expr ${NLINES} / 2`
+
+MARKERS=$(head -2 ${BATCH} | tail -1 | wc -w)
+
+sed s/INDIVIDUALSGOHERE/${INDIVIDS}/ ../../../../supreme-octo-disco/mainparams4h > mainparams
+sed -i s/MARKERSGOHERE/${MARKERS}/ mainparams
+
+
+for rep in `seq 1 20`
+do seq 2 2 ${NLINES} | shuf > ${rep}_random.txt
+head -1 nohead_${BATCH} > ${rep}_${BATCH}
+
+	for i in `cat ${rep}_random.txt`
+
+		do sed "${i}q;d" nohead_${BATCH} >> ${rep}_${BATCH}
+
+		next=$(( ${i}+1 ))
+
+		sed "${next}q;d" nohead_${BATCH} >> ${rep}_${BATCH}
+
+	done
+
+    qsub ../../../../supreme-octo-disco/Random_STRUCTURE.qsub -N ${rep}_STRUCTURE -t 3-22 -v thisfile=${rep}_${BATCH}
+
+    #qsub ../../../../supreme-octo-disco/Random_STRUCTURE_Long.qsub -N ${rep}_STRUCTURE -t 6-22 -v thisfile=${rep}_${BATCH}
+
+done
---
+++ 1.7_AllTheStructureParsing.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+## Just change number in line 11 to number of samples +1
+
+for i in `ls *_f`
+	do echo $i
+	grep -A 2 "Estimated Ln Prob of Data" $i
+	done > AlltheProbabilities.txt
+
+sed -i -E '/^([0-9]+)_.+-([0-9])+_f$/ N
+{/^([0-9]+)_.+-([0-9])+_f\nEstimated\sLn\sProb\sof\sData\s+\=\s(\-*[0-9]+\.*[0-9]+)$/ N
+{/^([0-9]+)_.+-([0-9])+_f\nEstimated\sLn\sProb\sof\sData\s+\=\s(\-*[0-9]+\.*[0-9]+)\nMean\svalue\sof\sln\slikelihood\s\=\s(\-*[0-9]+\.*[0-9]+)$/ N
+{s/^([0-9]+)_.+-([0-9])+_f\nEstimated\sLn\sProb\sof\sData\s+\=\s(\-*[0-9]+\.*[0-9]+)\nMean\svalue\sof\sln\slikelihood\s\=\s(\-*[0-9]+\.*[0-9]+)\nVariance\sof\sln\slikelihood\s+\=\s(\-*[0-9]+\.*[0-9]+)/\1,\2,\3,\4,\5/
+}
+}
+}' AlltheProbabilities.txt
+
+
+for i in `ls *_f`
+	do echo $i
+	grep -A 2 "Estimated Ln Prob of Data" $i
+	done > AlltheProbabilities.txt
+
+for i in `ls *_f`
+	do grep -A 259 "Inferred ancestry of individuals" $i > $i.forparse
+done
+
+for i in `ls *forparse`
+	do python ../../../../supreme-octo-disco/structureparse.py $i
+done
+
+mkdir parsed_data
+mkdir forparse
+mkdir raw_output
+mkdir error_output
+
+
+mv *_f.parsed parsed_data/
+mv *_f.forparse forparse
+mv *_f raw_output
+mv *.[eo]* error_output
