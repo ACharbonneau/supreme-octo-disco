6840c2812a41932a0e6c5b22203230cab9315091
Amanda Charbonneau <charbo24@msu.edu>
2017-04-02 22:35:03 -0400

    writing out names smartpca likes

--- SetupSmartPCA.R
+++ SetupSmartPCA.R
@@ -12,17 +12,23 @@ packages<-function(x){
 
 packages(dplyr)
 
-args = commandArgs(trailingOnly=TRUE)
+#args = commandArgs(trailingOnly=TRUE)
 
 #print(args[1])
+
+args="batch_20170402.structure.tsv"
 
 metadata <- read.csv("../../../../Metadata/SS_data.pop", header = F, sep="\t")
 stacksgenotypes <- data.table::fread(paste("../", args[1], sep = ""), header = F, sep="\t")
 stacksmarkers <- read.table(paste("../", args[1], sep = ""), nrows = 1, skip = 1)
+
+newbasename <- strsplit(args[1], split = ".structure.tsv")
+newbasename <- strsplit(newbasename[[1]], split = "batch_")[[1]][2]
+
 stacksmarkers <- as.data.frame(c("SSR", stacksmarkers))
 
-forbiallele <- paste(args[1], "_for_biallele.csv", sep = "")
-donebiallele <- paste(args[1], "_biallele.csv", sep="")
+forbiallele <- paste(newbasename, "_for_biallele.csv", sep = "")
+donebiallele <- paste(newbasename, "_biallele.csv", sep="")
 
 write.table(stacksmarkers, forbiallele, sep = ",", col.names = F, row.names = F, quote = F)
 
@@ -36,7 +42,7 @@ biallele <- read.csv(donebiallele)
 
 
 geno <- as.data.frame(t(select(biallele, -SSR)))
-write.table(geno, paste(args[1],".geno", sep = ""), sep = "", col.names = F, row.names = F, quote = F)
+write.table(geno, paste(newbasename,".geno", sep = ""), sep = "", col.names = F, row.names = F, quote = F)
 
 ind <- select(biallele, SSR)
 ind$SSR <- as.factor(ind$SSR)
@@ -44,7 +50,7 @@ ind <- left_join(ind, metadata, by=c("SSR"="V1"))
 ind$U <- "U"
 ind <- select(ind, SSR, U, V2)
 
-write.table(ind, paste(args[1],".ind", sep = ""), sep = "\t", col.names = F, row.names = F, quote = F)
+write.table(ind, paste(newbasename,".ind", sep = ""), sep = "\t", col.names = F, row.names = F, quote = F)
 
 snp <- colnames(biallele)
 snp <- as.data.frame(snp[2:length(snp)])
@@ -52,19 +58,19 @@ snp$FakeChromo <- c(1, 2, 3)
 snp$zero1 <- 0
 snp$zero2 <- 0
 
-write.table(snp, paste(args[1],".snp", sep = ""), sep = "\t", col.names = F, row.names = F, quote = F)
+write.table(snp, paste(newbasename,".snp", sep = ""), sep = "\t", col.names = F, row.names = F, quote = F)
 
 parnames <- c("genotypename:", "snpname:", "indivname:", "evecoutname:", 
               "evaloutname:", "altnormstyle:", "numoutevec:", "familynames:", 
               "grmoutname:", "snpweightoutname:", "genotypeoutname:", 
               "snpoutname:", "indivoutname:")
 
-parvalues <- c(paste(args[1],".geno", sep = ""), paste(args[1],".snp", sep = ""), 
-               paste(args[1],".ind", sep = ""), paste(args[1],".evec", sep = ""),
-               paste(args[1],".eval", sep = ""), "NO", length(snp$zero2), "NO",
-               paste(args[1],".grmjunk", sep = ""), paste(args[1],".weights", sep = ""), 
-               paste(args[1],".badgeno", sep = ""), paste(args[1],".badsnp", sep = ""),
-               paste(args[1],".badindiv", sep = ""))
+parvalues <- c(paste(newbasename,".geno", sep = ""), paste(newbasename,".snp", sep = ""), 
+               paste(newbasename,".ind", sep = ""), paste(newbasename,".evec", sep = ""),
+               paste(newbasename,".eval", sep = ""), "NO", length(snp$zero2), "NO",
+               paste(newbasename,".grmjunk", sep = ""), paste(newbasename,".weights", sep = ""), 
+               paste(newbasename,".badgeno", sep = ""), paste(newbasename,".badsnp", sep = ""),
+               paste(newbasename,".badindiv", sep = ""))
 parfile <- cbind(parnames, parvalues)
 
-write.table(parvalues, "smartpca.par", quote = F, sep = " ", row.names = F, col.names = F)
+write.table(parfile, "smartpca.par", quote = F, sep = " ", row.names = F, col.names = F)
