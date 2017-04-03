0f547f40b1b709554a959afae2cf825813b5c9c0
Amanda Charbonneau <charbo24@msu.edu>
2017-04-02 21:29:42 -0400

    need to load packages

--- SetupSmartPCA.R
+++ SetupSmartPCA.R
@@ -1,6 +1,17 @@
 
 #!/usr/bin/env Rscript
 
+# Install function for packages    
+packages<-function(x){
+  x<-as.character(match.call()[[2]])
+  if (!require(x,character.only=TRUE)){
+    install.packages(pkgs=x,repos="http://cran.r-project.org")
+    require(x,character.only=TRUE)
+  }
+}
+
+packages(dplyr)
+
 args = commandArgs(trailingOnly=TRUE)
 
 print(args[1])
