82313eb7f5d06f854c8ee1e4658f06d5f81839b7
Amanda Charbonneau <charbo24@msu.edu>
2017-04-02 21:49:19 -0400

    was missing reading in metadata

--- SetupSmartPCA.R
+++ SetupSmartPCA.R
@@ -16,6 +16,7 @@ args = commandArgs(trailingOnly=TRUE)
 
 print(args[1])
 
+metadata <- read.csv("../../../../Metadata/SigSelectionMeta.csv")
 stacksgenotypes <- data.table::fread(paste("../", args[1], sep = ""), header = F, sep="\t")
 stacksmarkers <- read.table(paste("../", args[1], sep = ""), nrows = 1, skip = 1)
 stacksmarkers <- as.data.frame(c("SSR", stacksmarkers))
@@ -27,9 +28,11 @@ write.table(stacksmarkers, forbiallele, sep = ",", col.names = F, row.names = F,
 
 write.table(select(stacksgenotypes, -V2), forbiallele, col.names = F, sep = ",", row.names = F, append = T, quote = F)
 
-system(paste("../../../../supreme-octo-disco/python bi-winning_allele.py", forbiallele, "-o", biallele, sep = " "), intern = TRUE,
+system(paste("../../../../supreme-octo-disco/python bi-winning_allele.py", forbiallele, "-o", donebiallele, sep = " "), intern = TRUE,
        ignore.stdout = FALSE, ignore.stderr = FALSE,
        wait = TRUE, input = NULL)
+
+biallele <- read.csv(donebiallele)
 
 
 geno <- as.data.frame(t(select(biallele, -SSR)))
@@ -37,9 +40,9 @@ write.table(geno, paste(args[1],".geno", sep = ""), sep = "", col.names = F, row
 
 ind <- select(biallele, SSR)
 ind$SSR <- as.factor(ind$SSR)
-ind <- left_join(ind, SSmeta, by=c("SSR"="V1"))
+ind <- left_join(ind, SSmeta, by=c("SSR"="ID"))
 ind$U <- "U"
-ind <- select(ind, SSR, U, V2)
+ind <- select(ind, SSR, U, Pop)
 
 write.table(ind, paste(args[1],".ind", sep = ""), sep = "\t", col.names = F, row.names = F, quote = F)
 
