3e844cdfad14c2384c99bbcdb775741056e533d0
Amanda Charbonneau <charbo24@msu.edu>
2017-05-02 16:23:17 -0400

    had wrong filename

--- 1.1_LaunchBuild.sh
+++ 1.1_LaunchBuild.sh
@@ -9,7 +9,7 @@
 #Change Derives_from from number.1 to number:
 less /mnt/research/radishGenomics/PublicData/2016RsativusGenome/Rs_1.0.Gene.LFY.gff.gz | sed -r s/\(Derives_from=Rs[0-9]+\)\.[0-9]/\\1/g > /mnt/research/radishGenomics/PublicData/2016RsativusGenome/Edited_Rs_1.0.Gene.LFY.gff
 
-qsub ../../supreme-octo-disco/1.22_build.qsub -N Jeong2016 -v genome=/mnt/research/radishGenomics/PublicData/2016RsativusGenome/Rs_1.0.chromosomes.fix.fasta,gff=/mnt/research/radishGenomics/PublicData/2016RsativusGenome/Edited_Rs_1.0.Gene.LFY.gff,gffi="Derives_from",exon="protein",stranded="yes"
+qsub ../../supreme-octo-disco/1.2_build.qsub -N Jeong2016 -v genome=/mnt/research/radishGenomics/PublicData/2016RsativusGenome/Rs_1.0.chromosomes.fix.fasta,gff=/mnt/research/radishGenomics/PublicData/2016RsativusGenome/Edited_Rs_1.0.Gene.LFY.gff,gffi="Derives_from",exon="protein",stranded="yes"
 
 
 #Sativus Genome: Draft sequences of the radish (Raphanus sativus L.) genome
@@ -20,7 +20,7 @@ qsub ../../supreme-octo-disco/1.22_build.qsub -N Jeong2016 -v genome=/mnt/resear
 #Get FASTA into right format
 awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < /mnt/research/radishGenomics/PublicData/RsativusGenome/RSA_r1.0 > /mnt/research/radishGenomics/PublicData/RsativusGenome/Edit_RSA_r1.0.fasta
 
-qsub ../../supreme-octo-disco/1.22_build.qsub -N Kitashiba2014 -v genome=/mnt/research/radishGenomics/PublicData/RsativusGenome/Edit_RSA_r1.0.fasta,gff=/mnt/research/radishGenomics/PublicData/RsativusGenome/RSA_r1.0_genes.gff3.gz,gffi="Parent",exon="exon",stranded="no"
+qsub ../../supreme-octo-disco/1.2_build.qsub -N Kitashiba2014 -v genome=/mnt/research/radishGenomics/PublicData/RsativusGenome/Edit_RSA_r1.0.fasta,gff=/mnt/research/radishGenomics/PublicData/RsativusGenome/RSA_r1.0_genes.gff3.gz,gffi="Parent",exon="exon",stranded="no"
 
 
 #Sativus Genome: Mitsui, Y., Shimomura, M., Komatsu, K., Namiki, N.,
@@ -28,7 +28,7 @@ qsub ../../supreme-octo-disco/1.22_build.qsub -N Kitashiba2014 -v genome=/mnt/re
 #gene expression profile of tuberous root formation and development.
 #Nature Publishing Group, 1–14. http://doi.org/10.1038/srep10835
 
-qsub ../../supreme-octo-disco/1.22_build.qsub -N Mitsui2015 -v genome=/mnt/research/radishGenomics/PublicData/2015_RsativusGenome/rsg_all_v1.fasta,gff=/mnt/research/radishGenomics/PublicData/2015_RsativusGenome/rsgv1.gff,gffi="Parent",exon="CDS",stranded="no"
+qsub ../../supreme-octo-disco/1.2_build.qsub -N Mitsui2015 -v genome=/mnt/research/radishGenomics/PublicData/2015_RsativusGenome/rsg_all_v1.fasta,gff=/mnt/research/radishGenomics/PublicData/2015_RsativusGenome/rsgv1.gff,gffi="Parent",exon="CDS",stranded="no"
 
 
 #Raphanistrum Genome: Moghe, G. D., Hufnagel, D. E., Tang, H., Xiao, Y.,
@@ -37,21 +37,21 @@ qsub ../../supreme-octo-disco/1.22_build.qsub -N Mitsui2015 -v genome=/mnt/resea
 #Raphanus raphanistrum and Three Other Brassicaceae Species. The Plant Cell
 #Online, 26(5), 1925–1937. http://doi.org/10.1105/tpc.114.124297
 
-qsub ../../supreme-octo-disco/1.22_build.qsub -N Moghe2014 -v genome=/mnt/research/radishGenomics/AnalysisOfSequencingFiles/MoghePublished/RrContigs.fa.fasta,gff=/mnt/research/radishGenomics/AnalysisOfSequencingFiles/MoghePublished/Rr_gene_pseu.gff.mod,gffi="Parent",exon="exon",stranded="no"
+qsub ../../supreme-octo-disco/1.2_build.qsub -N Moghe2014 -v genome=/mnt/research/radishGenomics/AnalysisOfSequencingFiles/MoghePublished/RrContigs.fa.fasta,gff=/mnt/research/radishGenomics/AnalysisOfSequencingFiles/MoghePublished/Rr_gene_pseu.gff.mod,gffi="Parent",exon="exon",stranded="no"
 
 
 #Raphanistrum transcriptome RR3_NY
 #ESTs
 
-qsub ../../supreme-octo-disco/1.22_build.qsub -N RR3_NY_EST -v genome=/mnt/research/radishGenomics/AnalysisOfSequencingFiles/EST_7pops/RR3_NY/1000017.est,gff=NA,gffi="NA",exon="NA",stranded="NA"
+qsub ../../supreme-octo-disco/1.2_build.qsub -N RR3_NY_EST -v genome=/mnt/research/radishGenomics/AnalysisOfSequencingFiles/EST_7pops/RR3_NY/1000017.est,gff=NA,gffi="NA",exon="NA",stranded="NA"
 
 
 #Brassica rapa genome
 
 
-qsub ../../supreme-octo-disco/1.22_build.qsub -N BR_2017 -v genome=/mnt/research/radishGenomics/PublicData/2017_Brapa/Brapa_R500_V1.2.fasta,gff=NA,gffi="NA",exon="NA",stranded="NA"
+qsub ../../supreme-octo-disco/1.2_build.qsub -N BR_2017 -v genome=/mnt/research/radishGenomics/PublicData/2017_Brapa/Brapa_R500_V1.2.fasta,gff=NA,gffi="NA",exon="NA",stranded="NA"
 
 
 #Arabidopsis thaliana genome
 
-qsub ../../supreme-octo-disco/1.22_build.qsub -N AT_2016 -v genome=/mnt/research/radishGenomics/PublicData/AT_TAIR10/TAIR10_chr_all.fas,gff=/mnt/research/radishGenomics/PublicData/AT_TAIR10/TAIR10_GFF3_genes.gff,gffi="Parent",exon="exon",stranded="no"
+qsub ../../supreme-octo-disco/1.2_build.qsub -N AT_2016 -v genome=/mnt/research/radishGenomics/PublicData/AT_TAIR10/TAIR10_chr_all.fas,gff=/mnt/research/radishGenomics/PublicData/AT_TAIR10/TAIR10_GFF3_genes.gff,gffi="Parent",exon="exon",stranded="no"
