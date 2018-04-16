0556dde334fb2d49334e32eb60236678d9cfcb61
Amanda Charbonneau <charbo24@msu.edu>
2018-04-16 10:09:52 -0400

    put done in wrong place in structure submit

--- 3_STRUCTURE_submit.sh
+++ 3_STRUCTURE_submit.sh
@@ -21,8 +21,8 @@ sed -i s/MARKERSGOHERE/${MARKERS}/ mainparams
 
 
 for rep in `seq 1 20`
-do seq 2 2 ${NLINES} | shuf > ${rep}_random.txt
-head -1 nohead_${BATCH} > ${rep}_${BATCH}
+	do seq 2 2 ${NLINES} | shuf > ${rep}_random.txt
+	head -1 nohead_${BATCH} > ${rep}_${BATCH}
 
 	for i in `cat ${rep}_random.txt`
 
@@ -33,5 +33,5 @@ head -1 nohead_${BATCH} > ${rep}_${BATCH}
 		sed "${next}q;d" nohead_${BATCH} >> ${rep}_${BATCH}
 
 	done
+	qsub ../../../supreme-octo-disco/3.1_Random_STRUCTURE.qsub -N ${rep}_STRUCTURE -t 3-${popmin} -v thisfile=${rep}_${BATCH}
 done
-qsub ../../../supreme-octo-disco/3.1_Random_STRUCTURE.qsub -N ${rep}_STRUCTURE -t 3-${popmin} -v thisfile=${rep}_${BATCH},depth=${depth}
